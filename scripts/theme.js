5cb89af1c5a5cbd574dc89c628a784ec4cee5dc4
SantiCombina <santicombina@gmail.com>
2024-08-24 13:38:47 -0300

    Update work section

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,17 +1,17 @@
-const themeToggleButton = document.getElementById('theme-toggle');
+const themeToggleButton = document.getElementById("theme-toggle");
 const body = document.body;
 
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme === 'dark') {
-    body.classList.add('dark-mode');
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+  body.classList.add("dark-mode");
 }
 
-themeToggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    
-    if (body.classList.contains('dark-mode')) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
+themeToggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
 });
